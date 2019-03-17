839f26d3c7a7eac5fac7cab423f65f3cc4922081
elyseeelius <elyseeelius@outlook.com>
2019-03-17 16:25:34 -0400

    final version

---
+++ song.js
@@ -0,0 +1,33 @@
+
+
+
+
+// My favorite song: Como un río en primavera. (Like a river in the springtime)
+
+
+/*This is my very first assigement for the
+course. It is about my favorite song .*/
+
+
+// This contains a string
+var songName = "Como río en primavera";
+
+// This variable contents an object
+var artist = {
+    name: "Marcela Gandala",
+    placeOfBirth:"Ciudad Juarez, Mexico",
+    age: 35,
+    married:true
+}
+var genre =  'Christian/Gospel';
+var album = 'El mismo cielo';
+
+// The two following variable contains only numbers as their value
+var durationInSeconds = 210;
+var released = 2009
+
+// This variable has a bollean value
+var spanish = 'true';
+
+// This variable ia about array
+var otherSongs =['Supe que me amaba', 'Pensaba en ti', 'Dame tu ojos', 'Vine adorarte']
